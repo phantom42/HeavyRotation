02752770c090fc328e1c19ac790c317b4896fa5d
Joe Coleman <phantom42@gmail.com>
2012-10-29 16:29:08 -0400

    Initial Commit

---
+++ HeavyRotation/RotationAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RotationAppDelegate.h
+//  HeavyRotation
+//
+//  Created by Joe Coleman on 10/29/12.
+//  Copyright (c) 2012 Joe Coleman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RotationAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HeavyRotation/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HeavyRotation
+//
+//  Created by Joe Coleman on 10/29/12.
+//  Copyright (c) 2012 Joe Coleman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RotationAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RotationAppDelegate class]));
+    }
+}
