831b82025f761fa705a3942b2873f614ebd18ddd
Joe Coleman <phantom42@gmail.com>
2012-10-29 17:08:12 -0400

    basic autorotation
    does not support upside-down orientation

--- HeavyRotation/RotationAppDelegate.m
+++ HeavyRotation/RotationAppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "RotationAppDelegate.h"
+#import "HeavyViewController.h"
 
 @implementation RotationAppDelegate
 
@@ -30,6 +31,9 @@
            selector:@selector(orientationChanged:)
                name:UIDeviceOrientationDidChangeNotification
              object:device] ;
+    
+    HeavyViewController *hvc = [[HeavyViewController alloc] init] ;
+    [[self window] setRootViewController:hvc] ;
     
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
