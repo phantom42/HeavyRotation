e658f83445ee9fdd6310ff6e0c7222cb8430ba68
Joe Coleman <phantom42@gmail.com>
2012-10-30 10:55:48 -0400

    completed main tutorial
    changed orientation to only work in landscape mode

--- HeavyRotation/HeavyViewController.m
+++ HeavyRotation/HeavyViewController.m
@@ -16,10 +16,7 @@
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)x
 {
-    // return YES if incoming orienation is portrait
-    // or either of hte landscapes. otherwise, return NO
-    return (x == UIInterfaceOrientationPortrait)
-    || UIInterfaceOrientationIsLandscape(x) ;
+    return UIInterfaceOrientationIsLandscape(x) ;
 }
 
 @end
